5a7cd7f7bef25abf055de0a032e75ccfc87e98b2
Thai Truong <sontruong@hotmail.co.uk>
2021-04-11 23:11:07 +0100

    header banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5ba4a4",
+        "light-cyan": "	#effafa",
+        "dark-cyan": "	#7b8e8e",
+        "v-dark-cyan": "#2c3a3a",
+      },
+    },
   },
   variants: {
     extend: {},
