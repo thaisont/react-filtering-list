715c2c927d4583b919b44bc8dffccc3a8595a97e
Thai Truong <sontruong@hotmail.co.uk>
2021-04-15 23:01:01 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from "./components/Header";
-import Jobs from "./components/Jobs";
 
 function App() {
   return (
     <div>
       <Header />
-      <Jobs />
     </div>
   );
 }
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,41 +1,65 @@
 import React from "react";
-import photo from "../photosnap.svg";
 
-function Jobs() {
+function Jobs({
+  title,
+  job,
+  latest,
+  featured,
+  img,
+  day,
+  duration,
+  location,
+  skillOne,
+  skillTwo,
+  skillThree,
+  skillFour,
+  skillFive,
+}) {
   return (
     <div className="max-w-screen-lg border-l-4 rounded border-primary m-auto  p-8 shadow bg-white">
       <div className="flex  gap-x-4">
         <div className="flex gap-x-4 mr-auto items-center">
-          <img src={photo} alt="" />
+          <img src={img} alt="" />
           <div className="text-sm flex flex-col">
             <div className="flex py-1 gap-x-4 items-center">
-              <p className="text-primary font-bold">Photosnap</p>
+              <p className="text-primary font-bold">{title}</p>
               <button className="text-xs rounded-full bg-primary text-white px-3 py-2 uppercase font-bold">
-                new!
+                {latest}
               </button>
               <button className="text-xs rounded-full bg-v-dark-cyan text-white px-3 py-2 uppercase">
-                featured
+                {featured}
               </button>
             </div>
             <div className="py-1">
-              <h2 className="font-bold text-lg">Senior Frontend Developer</h2>
+              <h2 className="font-bold text-lg">{job}</h2>
             </div>
             <div className="flex py-1 gap-x-8 text-dark-cyan">
-              <p>1d ago</p>
+              <p>{day}</p>
               <ul className="list-disc flex gap-x-8 ">
-                <li>Full time</li>
-                <li>USA only</li>
+                <li>{duration}</li>
+                <li>{location}</li>
               </ul>
             </div>
           </div>
         </div>
         <div className="flex items-center gap-x-4">
-          <p className="p-1.5 text-primary bg-light-cyan font-bold">Frontend</p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold"> Senior</p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold"> HTML</p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold"> CSS</p>
           <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            Javascript
+            {skillOne}
+          </p>
+          <p className="p-1.5 text-primary bg-light-cyan font-bold">
+            {" "}
+            {skillTwo}
+          </p>
+          <p className="p-1.5 text-primary bg-light-cyan font-bold">
+            {" "}
+            {skillThree}
+          </p>
+          <p className="p-1.5 text-primary bg-light-cyan font-bold">
+            {" "}
+            {skillFour}
+          </p>
+          <p className="p-1.5 text-primary bg-light-cyan font-bold">
+            {skillFive}
           </p>
         </div>
       </div>
