fb47333ed727e92597b64e9bf43e1117368ea9aa
Tan Ahmed <tannerahmed@gmail.com>
2021-04-21 09:27:51 +0100

    job listing improvement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import data from "./data.json";
-import JobBoard from "./components/JobBoard";
+import Jobs from "./components/Jobs";
 
 function App() {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    setJobs(data);
-  }, []);
-
-  console.log(jobs);
-
   return (
     <div>
       <Header />
-      <JobBoard />
-      {jobs.length === 0 ? (
-        <p>Jobs are fetching...</p>
-      ) : (
-        jobs.map((job) => <JobBoard job={job} key={job.id} />)
-      )}
+
+      {data && data.map((job) => <Jobs {...job} />)}
     </div>
   );
 }
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,67 +1,58 @@
 import React from "react";
 
 const Jobs = ({
-  title,
   role,
-  latest,
+  new: newRole,
   featured,
-  img,
-  day,
+  logo,
   duration,
   location,
+  position,
+  id,
+  company,
+  level,
+  postedAt,
+  contract,
+  languages,
+  tools,
 }) => {
   return (
     <div className="max-w-screen-lg rounded  m-auto  p-8 shadow bg-white mb-4">
-      <div className="flex  gap-x-4">
+      <div className="flex gap-x-4">
         <div className="flex gap-x-4 mr-auto items-center">
-          <img src={img} alt="" />
+          <img src={logo} alt="" />
           <div className="text-sm flex flex-col">
             <div className="flex py-1 gap-x-4 items-center">
-              <p className="text-primary font-bold">{title}</p>
-              <div className="text-xs rounded-full bg-primary text-white px-3 py-2 uppercase font-bold">
-                {latest}
-              </div>
-              <div className="text-xs rounded-full bg-v-dark-cyan text-white px-3 py-2 uppercase">
-                {featured}
-              </div>
+              <p className="text-primary font-bold">{company}</p>
+              {newRole && (
+                <div className="text-xs rounded-full bg-primary text-white px-3 py-2 uppercase font-bold">
+                  New!
+                </div>
+              )}
+              {featured && (
+                <div className="text-xs rounded-full bg-v-dark-cyan text-white px-3 py-2 uppercase">
+                  featured
+                </div>
+              )}
             </div>
             <div className="py-1">
-              <h2 className="font-bold text-lg">{role}</h2>
+              <h2 className="font-bold text-lg">{position}</h2>
             </div>
             <div className="flex py-1 gap-x-8 text-dark-cyan">
-              <p>{day}</p>
+              <p>{postedAt}</p>
               <ul className="list-disc flex gap-x-8 ">
-                <li>{duration}</li>
+                <li>{contract}</li>
                 <li>{location}</li>
               </ul>
             </div>
           </div>
         </div>
         <div className="flex items-center gap-x-4">
-          <button
-            className="p-1.5 text-primary bg-light-cyan font-bold"
-            data-role="frontend"
-          >
-            front end
-          </button>
-          {/* <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            {skillOne}
-          </p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            {" "}
-            {skillTwo}
-          </p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            {" "}
-            {skillThree}
-          </p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            {" "}
-            {skillFour}
-          </p>
-          <p className="p-1.5 text-primary bg-light-cyan font-bold">
-            {skillFive}
-          </p> */}
+          {languages.map((language) => (
+            <p className="p-1.5 text-xs text-primary bg-light-cyan font-bold">
+              {language}
+            </p>
+          ))}
         </div>
       </div>
     </div>
