c936b2edac7d42fd326c7d0cfcb28a475fcedeb4
Thai Truong <sontruong@hotmail.co.uk>
2021-07-27 23:34:07 +0100

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import data from "./data.json";
 import Jobs from "./components/Jobs";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Header />
-
+      <Search />
       {data && data.map((job) => <Jobs {...job} />)}
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <form className="form max-w-screen-lg py-2 m-auto">
+      <input type="search" className="query w-full rounded p-2" name="q" />
+      <button></button>
+    </form>
+  );
+};
+
+export default Search;
